89e31b75b85269f0229a002869d5afe66a857518
fmcarlucci <fabiom.carlucci@gmail.com>
2017-04-21 13:55:35 +0200

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,5 @@
 $('span.bibtex').click(function(){
-  // $('span.copyme').remove();
+  $('span.copyme').empty();
   var bibitem = $(this).parents('blockquote').next('.bibitem');
   bibitem.append('<span class="copyme">Click to copy</span>');
   bibitem.toggle();
